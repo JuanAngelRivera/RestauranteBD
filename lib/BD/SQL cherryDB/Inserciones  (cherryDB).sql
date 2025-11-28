0e32308b25da90ab4ee2c083768de67056b04cd4
Monserrat_PL <alexitadory97@gmail.com>
2025-11-28 11:38:51 -0600

    inserciones parte 2

--- lib/BD/SQL cherryDB/Inserciones  (cherryDB).sql
+++ lib/BD/SQL cherryDB/Inserciones  (cherryDB).sql
@@ -3,12 +3,111 @@ use cherryDB;
 /*delete  from TablaError
 where id != "miau";*/
 
--- select * from 	MetodoPago;
 INSERT INTO MetodoPago (descripcion) VALUES 
 ('Tarjeta.'),
 ('Efectivo');
+ select * from 	MetodoPago;
 
-select * from  CherryLocal;
 INSERT INTO CherryLocal (direccion,nombre,telefono) VALUES
-('Antonio Garcia Cubas,Nuevo Tecnologico.','CherryLocal','4611020976');
+('Antonio Garcia Cubas,Nuevo Tecnologico','CherryLocal','4611020976');
+select * from  CherryLocal;
+
+INSERT INTO Rol (descripcion, sueldo) VALUES 
+('Gerente', 2939.16),
+('Ayudante General', 1951.60);
+select * from Rol;
+
+
+INSERT INTO Turno (horaInicio, horaFin, descripcion) VALUES
+('07:00:00', '15:00:00', 'Turno Matutino'),
+('15:00:00', '21:00:00', 'Turno Vespertino ');
+select * from Turno;
+
+INSERT INTO Bonus (valor, descripcion) VALUES
+('100.00','Bonus por puntualidad');
+select * from Bonus;
+
+-- 
+
+
+INSERT INTO Categoria (nombre,descripcion) VALUES
+('Bebidas Calientes (Café)', 'Espresso, Americano, Lattes, Cappuccinos y métodos de extracción.'),
+('Otras Bebidas Calientes', 'Tés, infusiones, chocolate caliente y bebidas tradicionales no basadas en café.'),
+('Bebidas Frías', 'Iced Lattes, Frappés, Smoothies, jugos naturales y refrescos.'),
+('Panadería y Repostería', 'Pan dulce, croissants, muffins, panqués, galletas y postres varios.'),
+('Alimentos Salados', 'Sándwiches, paninis, bagels, quiches, empanadas y opciones de desayuno/comida.'),
+('Artículos Adicionales', 'Venta de café en grano/molido, tazas, termos y merchandising de la cafetería.');
+select * from Categoria;
+ 
+INSERT INTO Medida (descripcion) VALUES
+('Gramos'),
+('Mililitros'),
+('Piezas');
+select * from Medida;
+
+INSERT INTO Insumo (nombre, costo, descripcion, id_Medida, enAlmacen) VALUES
+('Café Molido', 180.00, 'Café tostado y molido', 1, 5000),
+('Agua', 10.00, 'Agua purificada', 2, 20000),
+('Leche', 25.00, 'Leche entera', 2, 15000),
+('Té Verde', 120.00, 'Té verde natural', 1, 2000),
+('Hielo', 5.00, 'Hielo en cubos', 1, 10000),
+('Chocolate', 90.00, 'Chocolate en polvo', 1, 3000),
+('Harina', 20.00, 'Harina de trigo', 1, 10000),
+('Huevo', 4.00, 'Huevo blanco', 3, 300),
+('Pan', 6.00, 'Pan blanco', 3, 200),
+('Jamón', 70.00, 'Jamón de pavo', 1, 3000),
+('Queso', 85.00, 'Queso manchego', 1, 3000),
+('Café en Grano', 200.00, 'Café sin moler', 1, 8000);
+select * from Insumo;
+
+
+INSERT INTO Producto (nombre, precio, id_Categoria, foto) VALUES
+('Café Americano', 35.00, 1, 'americano.jpg'),
+('Cappuccino', 55.00, 1, 'cappuccino.jpg'),
+('Té Verde', 30.00, 2, 'teverde.jpg'),
+('Frappé de Chocolate', 60.00, 3, 'frappe.jpg'),
+('Muffin de Chocolate', 28.00, 4, 'muffin.jpg'),
+('Sándwich Jamón y Queso', 50.00, 5, 'sandwich.jpg'),
+('Café en Grano 250g', 120.00, 6, 'cafegrano.jpg');
+select* from Producto;
+
+
+INSERT INTO Ingredientes (id_Producto, id_Insumo, cantidad) VALUES
+-- Café Americano
+(1, 1, 10),  -- Café Molido
+(1, 2, 250), -- Agua
+-- Cappuccino
+(2, 1, 10),  -- Café Molido
+(2, 3, 200), -- Leche
+-- Té Verde
+(3, 4, 5),   -- Té Verde
+(3, 2, 250), -- Agua
+-- Frappé
+(4, 3, 200), -- Leche
+(4, 5, 150), -- Hielo
+(4, 6, 20),  -- Chocolate
+-- Muffin
+(5, 7, 100), -- Harina
+(5, 8, 1),   -- Huevo
+(5, 6, 30),  -- Chocolate
+-- Sándwich
+(6, 9, 2),   -- Pan
+(6,10,50),   -- Jamón
+(6,11,40),   -- Queso
+-- Café en Grano
+ (7,12,250);  -- Café en grano
+
+select * from Ingredientes;
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lib/BD/SQL cherryDB/cherryDB.sql
+++ lib/BD/SQL cherryDB/cherryDB.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS cherryDB;
+
 
 create  database cherryDB;
 use cherryDB;
@@ -5,25 +7,25 @@ use cherryDB;
 CREATE TABLE TablaError (id varchar(20) NOT NULL,
 					descripcion TEXT,
                     constraint ErrorPK primary key (id)
-                    ); -- 🐑
+                    ); -- 🐑	🐑
 
 CREATE TABLE MetodoPago (id INTEGER AUTO_INCREMENT not null,
 						 descripcion TEXT,
                          constraint MetodopagoPK primary key (id)
-                         ); -- 🐑
+                         ); -- 🐑	🐑
 
 CREATE TABLE CherryLocal (id INTEGER AUTO_INCREMENT NOT NULL,
-					direccion VARCHAR(45),
-                    nombre VARCHAR(25),
-                    telefono VARCHAR(10),
+					direccion VARCHAR(50),
+                    nombre VARCHAR(40),
+                    telefono VARCHAR(15),
                     constraint LocalPK primary key (id)
-                    );  -- 🐑
+                    );  -- 🐑	🐑
 
 CREATE TABLE Rol (id INTEGER AUTO_INCREMENT NOT NULL,
 				  descripcion TEXT,
                   sueldo DECIMAL(9,2),
                   constraint RolPK primary key (id) 
-                  ); -- 🐑
+                  ); -- 🐑	🐑
 
 
 CREATE TABLE Turno (id INTEGER AUTO_INCREMENT NOT NULL,
@@ -31,17 +33,17 @@ CREATE TABLE Turno (id INTEGER AUTO_INCREMENT NOT NULL,
 					horaFin TIME,
 					descripcion TEXT,
 					constraint TurnoPK primary key (id)
-                    ); -- 🐑
+                    ); -- 🐑	🐑
 
 CREATE TABLE Bonus (id INTEGER AUTO_INCREMENT NOT NULL,
 					valor DECIMAL(9,2),
                     descripcion TEXT,
                     constraint BonusPK primary key (id)
-                    ); -- 🐑
+                    ); -- 🐑    no
                     
 CREATE TABLE Empleado (id INTEGER AUTO_INCREMENT NOT NULL,
-					   nombre VARCHAR(25),
-                       telefono VARCHAR(10),
+					   nombre VARCHAR(40),
+                       telefono VARCHAR(15),
                        CURP VARCHAR(18) UNIQUE,
                        RFC VARCHAR(13) UNIQUE,
                        fechaIngreso DATE,
@@ -52,14 +54,14 @@ CREATE TABLE Empleado (id INTEGER AUTO_INCREMENT NOT NULL,
                        constraint EmpleadoPK primary key (id),
                        constraint EmpleadoFK1 foreign key (Id_Rol) REFERENCES Rol(id),
                        constraint EmpleadoFK2 foreign key (Id_Turno) REFERENCES Turno(Id)
-                       ); -- 🐑
+                       ); -- 🐑     no
                        
 CREATE TABLE Nomina (id_Empleado INTEGER NOT NULL,
 					 fechaPago DATE NOT NULL,
                      salarioTotal DECIMAL(9,2),
                      constraint NominaPK primary key (id_Empleado, fechaPago),
                      constraint NominaFK foreign key (id_Empleado) REFERENCES Empleado(id)
-                     );   -- 🐑
+                     );   -- 🐑   no
 
 CREATE TABLE Beneficios (id_Empleado INTEGER NOT NULL,
                          fechaPago_Nomina DATE NOT NULL,
@@ -67,24 +69,24 @@ CREATE TABLE Beneficios (id_Empleado INTEGER NOT NULL,
                          constraint primary key (id_Empleado, fechaPago_Nomina, id_Bonus),
                          constraint BeneficiosFK1 foreign key (id_Empleado, fechaPago_Nomina) REFERENCES Nomina(id_Empleado, fechaPago),
                          constraint BeneficiosFK2 foreign key (id_Bonus) REFERENCES Bonus(id)
-); -- 🐑
+                         ); -- 🐑 no 
 
 CREATE TABLE Cuenta (id_Empleado INTEGER NOT NULL,
 					 nombreUsuario VARCHAR(25) NOT NULL,
                      contraseña VARCHAR(25),
                      constraint CuentaPK primary key (id_Empleado, nombreUsuario),
                      constraint CuentaFK1 foreign key (id_Empleado) REFERENCES Empleado(id)
-                     ); -- 🐑
+                     ); -- 🐑	no
 
 CREATE TABLE Orden (id_CherryLocal INTEGER,
-					id INTEGER AUTO_INCREMENT NOT NULL,
+				id INTEGER AUTO_INCREMENT NOT NULL,
                     total DECIMAL(9,2),
                     fechaRealizada DATE,
                     id_Empleado INTEGER,
                     constraint OrdenPK primary key(id,id_CherryLocal),
                     constraint OrdenFK1 foreign key (id_CherryLocal) REFERENCES CherryLocal(id),
                     constraint OrdenFK2 foreign key (id_Empleado) REFERENCES Empleado(id)
-                    ); -- 🐑
+                    ); -- 🐑 	no
                     
 CREATE TABLE Pago (id INTEGER AUTO_INCREMENT NOT NULL,
 				   descripcion TEXT,
@@ -94,22 +96,22 @@ CREATE TABLE Pago (id INTEGER AUTO_INCREMENT NOT NULL,
                    constraint PagoPK primary key (id),
                    constraint PagoFK1 foreign key (id_metodoPago) references MetodoPago(id),
                    constraint PagoFK2 foreign key (id_orden,id_CherryLocal) references Orden(id,id_CherryLocal)
-                   ); -- 🐑
+                   ); -- 🐑	no
                     
 CREATE TABLE Categoria (id INTEGER AUTO_INCREMENT NOT NULL,
-						nombre VARCHAR(25),
+						nombre VARCHAR(40),
                         descripcion TEXT,
                         constraint CategoriaPK primary key (id)
-                        ); -- 🐑
+                        ); -- 🐑	🐑
 
 CREATE TABLE Producto (id INTEGER AUTO_INCREMENT NOT NULL,
-					   nombre VARCHAR(25),
+					   nombre VARCHAR(40),
                        precio DECIMAL(9,2),
                        id_Categoria INTEGER,
                        foto VARCHAR(100),
                        constraint ProductoPK primary key (id),
                        constraint ProductoFK FOREIGN KEY (Id_Categoria) REFERENCES Categoria(Id)
-                       ); -- 🐑
+                       ); -- 🐑   🐑
 
 CREATE TABLE Contiene (id_Orden INTEGER,
 					   id_CherryLocal INTEGER,
@@ -117,7 +119,7 @@ CREATE TABLE Contiene (id_Orden INTEGER,
                        constraint ContienePK primary key (id_Orden, id_CherryLocal, id_Producto),
                        constraint ContieneFK1 foreign key (id_Orden, id_CherryLocal) REFERENCES Orden(id,id_CherryLocal),
                        constraint ContieneFK2 foreign key (id_Producto) REFERENCES Producto(id)
-                       ); -- 🐑
+                       ); -- 🐑  no
 
 CREATE TABLE Descuento (id INTEGER AUTO_INCREMENT NOT NULL,
 						descripcion TEXT,
@@ -125,29 +127,29 @@ CREATE TABLE Descuento (id INTEGER AUTO_INCREMENT NOT NULL,
                         id_Producto INTEGER,
                         constraint DescuentoPK primary key (id),
                         constraint DescuentoFK foreign key (id_Producto) REFERENCES Producto(id)
-                        ); -- 🐑
+                        ); -- 🐑   
                         
 CREATE TABLE Medida (id INTEGER AUTO_INCREMENT NOT NULL,
 					descripcion TEXT,
                     constraint MedidaPK primary key (id)
-                    ); -- 🐑
+                    ); -- 🐑	 🐑
 
 CREATE TABLE Proveedor (id INTEGER AUTO_INCREMENT NOT NULL,
-					    nombre VARCHAR(25),
-                        direccion VARCHAR(30),
+					    nombre VARCHAR(40),
+                        direccion VARCHAR(50),
                         descripcion TEXT,
                         constraint ProveedorPK primary key (id)
                         ); -- 🐑
 
 CREATE TABLE Insumo (id INTEGER AUTO_INCREMENT NOT NULL,
-					 nombre VARCHAR(25),
+					 nombre VARCHAR(40),
                      costo DECIMAL(9,2),
                      descripcion TEXT,
                      id_Medida INTEGER,
                      enAlmacen INTEGER,
                      constraint InsumoPK primary key (id),
                      constraint InsumoFK1 foreign key (id_Medida) REFERENCES Medida(id)
-                     ); -- 🐑
+                     ); -- 🐑	 🐑
 
 CREATE TABLE Ingredientes (id_Producto INTEGER,
                            id_Insumo INTEGER,
@@ -155,12 +157,12 @@ CREATE TABLE Ingredientes (id_Producto INTEGER,
                            constraint primary key (id_Insumo, id_Producto),
                            constraint IngredientesFK1 foreign key (id_Insumo) REFERENCES Insumo(id),
                            constraint IngredientesFK2 foreign key (id_Producto) REFERENCES Producto(id)
-                           ); -- 🐑
+                           ); -- 🐑   🐑
 
 CREATE TABLE Contacto (id_Proveedor INTEGER,
 					   numero INTEGER NOT NULL,
-                       nombre VARCHAR(25),
-                       telefono VARCHAR(10),
+                       nombre VARCHAR(40),
+                       telefono VARCHAR(15),
                        constraint primary key (id_Proveedor, numero),
                        CONSTRAINT ContactoFK1 foreign key (id_Proveedor) REFERENCES Proveedor(id)
                        );          
